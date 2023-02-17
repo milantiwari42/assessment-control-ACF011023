963418fa32797a7272d074b33e128296b0e5a4ad
iovv13nat <117558195+iovv13nat@users.noreply.github.com>
2023-02-17 15:52:24 -0800

    NataliaK:ACF_955_GroupCode_TestCases

---
+++ src/test/java/definitions/Nk_XPathLibrary.java
@@ -0,0 +1,7 @@
+package definitions;
+
+public class Nk_XPathLibrary {
+    public static String sRegisterMeButtonLocation ="//button[@type='submit']";
+    public static String sErrorMessageLocation ="//mat-form-field[4]//mat-error[1]";
+    public static String sGroupCodeLocation ="//input[@placeholder='Group Code']";
+}
